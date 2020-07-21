de36c585d934c6f2215bcfd1a3785ab07b9b1e76
Harley Hanna <harleyhannawrit@gmail.com>
2020-07-21 22:17:39 +0000

    added scrape_from_list_page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,20 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
+
 class Scraper
-  
-  
-  def initialize
-    
-    
-  end
-  
-  
+
+
+  def scrape_from_list_page(url) #scrapes site for information about each game
+
+    html = Nokogiri::HTML(open(url, 'User-Agent' => 'firefox')) #scrape site and convert to HTML
+
+    html.css("table.clamp list td").each do |game| #each enumerator to gather information about each game
+
+    end
+
+
+
 end
