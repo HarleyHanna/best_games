15ea0d3325df8f909beee143f8053c84c6a6a327
Harley Hanna <harleyhannawrit@gmail.com>
2020-07-21 22:49:54 +0000

    added assignment of attributes to scrape method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,13 +7,25 @@ require 'open-uri'
 class Scraper
 
 
-  def scrape_from_list_page(url) #scrapes site for information about each game
+
+
+  def self.scrape_from_list_page(url) #scrapes site for information about each game
 
     html = Nokogiri::HTML(open(url, 'User-Agent' => 'firefox')) #scrape site and convert to HTML
+    games = []
+    counter = 0
 
-    html.css("table.clamp list td").each do |game| #each enumerator to gather information about each game
+      html.css("table.clamp-list td").each do |game| #each enumerator to gather information about each game
+          game_details = {}
+          game_details[:score] = game.css()
+          game_details[:name] = game.css()
+          game_details[:description] = game.css()
+          games << game_details
+          binding.pry
+      end
+    puts games
+  end
 
-    end
 
 
 
