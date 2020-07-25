12ce554e77928b3011a4a674c2570dc37f11c2e5
Harley Hanna <harleyhannawrit@gmail.com>
2020-07-25 15:26:18 +0000

    scraper class only scrapes first top 10 games

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,14 +12,18 @@ class Scraper
     games = []
     counter = 0
     page = Nokogiri::HTML(open(url, 'User-Agent' => 'firefox'))
-    page.css("td.clamp-summary-wrap").each do |game| #iterates over each game, collecting score, description, title, and platform
-      game_details = {}
-      game_details[:score] = game.css("div.clamp-score-wrap div.metascore_w.large.game.positive").text.strip
-      game_details[:description] = game.css("div.summary").text.strip
-      game_details[:title] = game.css("a.title").text.strip
-      game_details[:platform] = game.css("div.clamp-details div.platform span.data").text.strip
-      games << game_details
-    end
+      page.css("td.clamp-summary-wrap").each do |game| #iterates over each game, collecting score, description, title, and platform
+        game_details = {}
+        game_details[:score] = game.css("div.clamp-score-wrap div.metascore_w.large.game.positive").text.strip
+        game_details[:description] = game.css("div.summary").text.strip
+        game_details[:title] = game.css("a.title").text.strip
+        game_details[:platform] = game.css("div.clamp-details div.platform span.data").text.strip
+        games << game_details
+        counter += 1
+        if counter == 10
+          break
+        end
+      end
     games
   end
 
